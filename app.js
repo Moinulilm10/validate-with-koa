3043e8615be52218de333fe6ce195bdeb2396953
Moinul Islam <moinulilm10@gmail.com>
2024-04-07 17:51:29 +0600

    validator added

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
-const Validator = require("validatorjs");
+const validator = require("validatorjs");
 const app = new Koa();
 
 const PORT = 8080;
 
 app.use(bodyParser());
+
+const rules = {
+  id: "required|integer",
+  name: "required|string",
+  email: "required|email",
+  age: "required|integer|min:18",
+};
+
+app.use((ctx) => {
+  const data = ctx.request.body;
+  const validation = new validator(data, rules);
+  if (validation.passes()) {
+    ctx.status = 200;
+    // console.log(ctx.status);
+    ctx.body = `ID: ${data.id} Name: ${data.name} Email: ${data.email} Age: ${data.age}`;
+  } else {
+    ctx.status = 400;
+    // console.log(ctx.status);
+    ctx.body = "Info are not valid";
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
